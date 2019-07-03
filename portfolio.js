a7e46e61e09fe34b9178740a26a3496bc8eb707a
Rebecca Viner <rtviner@gmail.com>
2019-07-03 13:14:46 -0600

    add toggle functionality to info button to to change button icon and overlay view on click

--- portfolio.js
+++ portfolio.js
@@ -1,11 +1,33 @@
-let projects = document.getElementById('projectExamples');
+let infoButtons = document.querySelectorAll('.projectLink.info');
 
-projects.addEventListener('click', function(event) {
-	if (event.target.className === "projectLink info") {
-		let overlay = event.target.nextElementSibling.lastElementChild;
-		overlay.classList.add('clicked');
-	} else if (event.target.className == "close") {
-		let overlay = event.target.parentNode;
-		overlay.classList.remove('clicked');
-	}
-});
+function changeIcon(currentClass, icon) {
+	let newClass = (currentClass === "fa fa-info-circle") ?
+			"fa fa-close" :
+			"fa fa-info-circle";
+	icon.className = newClass;
+}
+
+function changeOverlayView(currentView, overlay) {
+	let newClass = (currentView === "overlay hidden") ?
+			"overlay visible" :
+			"overlay hidden";
+	overlay.className = newClass;
+}
+
+function handleButtonClick (event) {
+	let overlay = event.target.nextElementSibling.lastElementChild;
+	let icon = event.target.firstElementChild;
+	changeIcon(icon.className, icon);
+	changeOverlayView(overlay.className, overlay);
+}
+
+function handleIconClick (event) {
+	let overlay = event.target.parentElement.nextElementSibling.lastElementChild;
+	let icon = event.target;
+	changeIcon(icon.className, icon);
+	changeOverlayView(overlay.className, overlay);
+}
+
+infoButtons.forEach(button => button.addEventListener('click', event => (event.target.id === "open") ?
+		handleButtonClick(event) : handleIconClick(event)
+));
